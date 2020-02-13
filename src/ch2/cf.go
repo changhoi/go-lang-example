a3e6270eb324a512950adc0c714b187b734347cd
changhoi <0417kch@naver.com>
2020-02-13 23:34:14 +0900

    ch2

---
+++ src/ch2/cf.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		t, err := strconv.ParseFloat(arg, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+			os.Exit(1)
+		}
+	}
+}
